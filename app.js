cee3e513678860a5744e6871e51bfb2c93dee08a
Sahil-3210 <140245901+Sahil-3210@users.noreply.github.com>
2024-03-26 10:17:16 +0530

    need update to JS file

---
+++ app.js
@@ -0,0 +1,44 @@
+const BASE_URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/eur.json"
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("#rateBtn");
+
+
+
+for(let select of dropdowns) {
+    for(currCode in countryList){
+        let newOption = document.createElement("option");
+        newOption.innerHTML = currCode;
+        newOption.value = currCode;
+        if(select.name === 'from' && currCode === 'USD'){
+            newOption.selected = "selected";
+        }
+        else if(select.name === 'to' && currCode === 'INR'){
+            newOption.selected = "selected";
+        }
+        select.append(newOption);
+    }
+    select.addEventListener("change", (evt) =>{
+        updateFlag(evt.target);
+    });
+}
+
+const updateFlag = (element) =>{
+    let currCode = element.value;
+    let countryCode = countryList[currCode];
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+    let img = element.parentElement.querySelector("img");
+    img.src = newSrc;
+}
+
+btn.addEventListener("click", (evt)=>{
+    evt.preventDefault();
+    let amount = document.querySelector(".amount input");
+    let amtVal = amount.value;
+    if(amtVal === "" || amtVal < 1){
+        amtVal = 1;
+        amtVal.value = "1";
+
+    }
+    
+});
